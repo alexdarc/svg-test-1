9690c6f9b54b35a9f6aae5a3546ed027f364dc0f
Andrey Chizh <avchizh@gmail.com>
2019-07-28 18:36:30 +0300

    переработал sequence-flow.component

--- src/app/board/elements/sequence-flow/sequence-flow.component.ts
+++ src/app/board/elements/sequence-flow/sequence-flow.component.ts
@@ -1,47 +1,59 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Renderer2 } from '@angular/core';
 
 import { IProcessComponent } from '../../shared/models/process-component.model';
 import { SequenceFlow } from './sequence-flow.model';
+import { CompileShallowModuleMetadata } from '@angular/compiler';
+import { CdkDragMove, CdkDragEnter, CdkDrag } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'svg:svg[app-sequence-flow]',
-  template: `
-    <svg>
-      <defs>
-        <marker
-          id="flow-end"
-          viewBox="0 0 20 20"
-          refX="11"
-          refY="10"
-          markerWidth="10"
-          markerHeight="10"
-          orient="auto"
-        >
-          <path class="marker" d="M 1 5 L 11 10 L 1 15 Z"></path>
-        </marker>
-      </defs>
-      <path class="line" attr.d="m {{ context.waypoints | waypoints }} "></path>
-    </svg>
-  `,
-  styles: [`
-    .line {
-      fill: none;
-      stroke-width: 2px;
-      stroke: black;
-      stroke-linejoin: round;
-      marker-end: url('#flow-end');
-    }
-
-    .marker {
-      fill: black;
-      stroke-width: 1px;
-      stroke-linecap: round;
-      stroke-dasharray: 10000, 1;
-      stroke: black;
-    }
-  `],
+  templateUrl:'./sequence-flow.component.html',
+  styleUrls: ['./sequence-flow.component.css'],
 })
 export class SequenceFlowComponent implements IProcessComponent {
   @Input()
   context: SequenceFlow;
+
+  constructor(
+    private renderer: Renderer2,
+  )
+  {
+    
+  }
+
+  dragEntered(event: CdkDragEnter)
+  {
+    console.log(event);
+  }
+
+  mousedown(event:any)
+  {
+    console.log(event);
+    this.renderer.setStyle(
+      event.source.element.nativeElement,
+      'pointer-events',
+      'none'
+    );
+  }
+
+  waypointMove(event: CdkDragMove, index: number)
+  {
+    this.context.waypoints[index].x = event.event["offsetX"];
+    this.context.waypoints[index].y = event.event["offsetY"];
+
+    /*
+      NOTE: Библиотека drag-n-drop работает как-то не так
+      reset очищает стили transform, но не удаляет аттрибут transform.
+      Возможно это из-за того что svg, но почему тогда этот аттрибут добавляется?
+    */
+    this.renderer.removeAttribute(
+      event.source.element.nativeElement,
+      'transform',
+    );
+
+    this.renderer.removeStyle(
+      event.source.element.nativeElement,
+      'transform',
+    );
+  }
 }
