69dee2c993ca2f78dbe8acf7192ca0cdce15fe72
Andrey Chizh <avchizh@gmail.com>
2019-07-28 18:39:52 +0300

    Почистил код

--- src/app/board/elements/sequence-flow/sequence-flow.component.ts
+++ src/app/board/elements/sequence-flow/sequence-flow.component.ts
@@ -1,9 +1,7 @@
-import { Component, OnInit, Input, Renderer2 } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { IProcessComponent } from '../../shared/models/process-component.model';
 import { SequenceFlow } from './sequence-flow.model';
-import { CompileShallowModuleMetadata } from '@angular/compiler';
-import { CdkDragMove, CdkDragEnter, CdkDrag } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'svg:svg[app-sequence-flow]',
@@ -13,47 +11,4 @@ import { CdkDragMove, CdkDragEnter, CdkDrag } from '@angular/cdk/drag-drop';
 export class SequenceFlowComponent implements IProcessComponent {
   @Input()
   context: SequenceFlow;
-
-  constructor(
-    private renderer: Renderer2,
-  )
-  {
-    
-  }
-
-  dragEntered(event: CdkDragEnter)
-  {
-    console.log(event);
-  }
-
-  mousedown(event:any)
-  {
-    console.log(event);
-    this.renderer.setStyle(
-      event.source.element.nativeElement,
-      'pointer-events',
-      'none'
-    );
-  }
-
-  waypointMove(event: CdkDragMove, index: number)
-  {
-    this.context.waypoints[index].x = event.event["offsetX"];
-    this.context.waypoints[index].y = event.event["offsetY"];
-
-    /*
-      NOTE: Библиотека drag-n-drop работает как-то не так
-      reset очищает стили transform, но не удаляет аттрибут transform.
-      Возможно это из-за того что svg, но почему тогда этот аттрибут добавляется?
-    */
-    this.renderer.removeAttribute(
-      event.source.element.nativeElement,
-      'transform',
-    );
-
-    this.renderer.removeStyle(
-      event.source.element.nativeElement,
-      'transform',
-    );
-  }
 }
