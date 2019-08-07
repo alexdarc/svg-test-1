3684fd9848b0219eb8d9c825ebe46ab0eb8057d2
Ivasek <ivasekthegreat@gmailc.om>
2019-08-08 00:52:31 +0300

    prettify tests

--- BPMN/BL/ConnectionRules/SequenceFlowConnectionRules.spec.ts
+++ BPMN/BL/ConnectionRules/SequenceFlowConnectionRules.spec.ts
@@ -1,9 +1,20 @@
 describe('SequenceFlowConnectionRules (spec 7.6.1)', () => {
+  it('IntermediateEvent has incoming SequenceFlow', () => { throw new Error('not implemented'); });
+  it('IntermediateEvent has outgoing SequenceFlow', () => { throw new Error('not implemented'); });
 
-  // todo split this test to more cases
-  it('FlowObject can be connected to another', () => { throw new Error('not implemented'); });
   it('StartEvent has no incoming SequenceFlows', () => { throw new Error('not implemented'); });
+  it('StartEvent has outgoing SequenceFlow', () => { throw new Error('not implemented'); });
+
+  it('EndEvent has incoming SequenceFlow', () => { throw new Error('not implemented'); });
   it('EndEvent has no outgoing SequenceFlows', () => { throw new Error('not implemented'); });
+
+  it('Activity has incoming SequenceFlow', () => { throw new Error('not implemented'); });
+  it('Activity has outgoing SequenceFlow', () => { throw new Error('not implemented'); });
+
+  it('Gateway has incoming SequenceFlow', () => { throw new Error('not implemented'); });
+  it('Gateway has outgoing SequenceFlow', () => { throw new Error('not implemented'); });
+
   it('SubProcess can not be connected to FlowObjects outside of SubProcess', () => { throw new Error('not implemented'); });
+
   it('SequenceFlow can not cross Pool boundary', () => { throw new Error('not implemented'); });
 });
